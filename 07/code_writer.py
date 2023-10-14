d636b3d02aa02f4f0abfd33162f00a30d8ae3c62
Dan Cao <git@cao.is>
2023-10-14 17:28:38 +0100

    07 - add arithmetic commands to vm translator for StackTest

--- 07/code_writer.py
+++ 07/code_writer.py
@@ -5,12 +5,19 @@ class CodeWriter:
     def __init__(self):
         self.output = []
         self.file_name = ""
+        self.label_counter = 0
 
     def get_output(self):
         return self.output
 
     def set_file_name(self, file_name):
         self.file_name = file_name
+        self.label_counter = 0
+
+    def new_label(self):
+        label = f"DAN{self.label_counter}DAN"
+        self.label_counter += 1
+        return label
 
     def write_arithmetic(self, command):
         self.output.append(f"// {command}")
@@ -24,28 +31,155 @@ class CodeWriter:
                         "D=M",
                         # Add D to value below in stack
                         "A=A-1",
-                        "M=M+D",
+                        "M=D+M",
                         # Decrement stack pointer,
                         "@SP",
                         "M=M-1",
                     ]
                 )
             case "sub":
-                pass
+                self.output.extend(
+                    [
+                        # Get value from top of stack, load into D
+                        "@SP",
+                        "A=M-1",
+                        "D=M",
+                        # Sub D from value below in stack
+                        "A=A-1",
+                        "M=M-D",
+                        # Decrement stack pointer,
+                        "@SP",
+                        "M=M-1",
+                    ]
+                )
             case "neg":
-                pass
+                self.output.extend(
+                    [
+                        # Get value from top of stack
+                        "@SP",
+                        "A=M-1",
+                        # Apply arithmetic negation
+                        "M=-M",
+                    ]
+                )
             case "eq":
-                pass
+                done_label = self.new_label()
+                self.output.extend(
+                    [
+                        # Get value from top of stack, load into D
+                        "@SP",
+                        "A=M-1",
+                        "D=M",
+                        # Do x - y, store result in D
+                        "A=A-1",
+                        "D=M-D",
+                        # Set initial result to true (-1)
+                        "M=-1",
+                        # Decrement stack pointer,
+                        "@SP",
+                        "M=M-1",
+                        # We are done if x - y == 0
+                        f"@{done_label}",
+                        "D;JEQ",
+                        # Otherwise result is false (0)
+                        "@SP",
+                        "A=M-1",
+                        "M=0",
+                        f"({done_label})",
+                    ]
+                )
             case "gt":
-                pass
+                done_label = self.new_label()
+                self.output.extend(
+                    [
+                        # Get value from top of stack, load into D
+                        "@SP",
+                        "A=M-1",
+                        "D=M",
+                        # Do x - y, store result in D
+                        "A=A-1",
+                        "D=M-D",
+                        # Set initial result to true (-1)
+                        "M=-1",
+                        # Decrement stack pointer,
+                        "@SP",
+                        "M=M-1",
+                        # We are done if x - y > 0
+                        f"@{done_label}",
+                        "D;JGT",
+                        # Otherwise result is false (0)
+                        "@SP",
+                        "A=M-1",
+                        "M=0",
+                        f"({done_label})",
+                    ]
+                )
             case "lt":
-                pass
+                done_label = self.new_label()
+                self.output.extend(
+                    [
+                        # Get value from top of stack, load into D
+                        "@SP",
+                        "A=M-1",
+                        "D=M",
+                        # Do x - y, store result in D
+                        "A=A-1",
+                        "D=M-D",
+                        # Set initial result to true (-1)
+                        "M=-1",
+                        # Decrement stack pointer,
+                        "@SP",
+                        "M=M-1",
+                        # We are done if x - y < 0
+                        f"@{done_label}",
+                        "D;JLT",
+                        # Otherwise result is false (0)
+                        "@SP",
+                        "A=M-1",
+                        "M=0",
+                        f"({done_label})",
+                    ]
+                )
             case "and":
-                pass
+                self.output.extend(
+                    [
+                        # Get value from top of stack, load into D
+                        "@SP",
+                        "A=M-1",
+                        "D=M",
+                        # Bitwise AND with D and value below in stack
+                        "A=A-1",
+                        "M=D&M",
+                        # Decrement stack pointer,
+                        "@SP",
+                        "M=M-1",
+                    ]
+                )
             case "or":
-                pass
+                self.output.extend(
+                    [
+                        # Get value from top of stack, load into D
+                        "@SP",
+                        "A=M-1",
+                        "D=M",
+                        # Bitwise OR with D and value below in stack
+                        "A=A-1",
+                        "M=D|M",
+                        # Decrement stack pointer,
+                        "@SP",
+                        "M=M-1",
+                    ]
+                )
             case "not":
-                pass
+                self.output.extend(
+                    [
+                        # Get value from top of stack
+                        "@SP",
+                        "A=M-1",
+                        # Apply bitwise not
+                        "M=!M",
+                    ]
+                )
             case _:
                 raise NotImplementedError(f"Unsupported arithmetic command {command}")
 
